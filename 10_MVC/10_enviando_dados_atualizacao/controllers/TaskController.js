fb69a252798abf51d876e5e5c5d3d7126eb6f9d1
Ian Pereira Caminhas <iancaminhas25@hotmail.com>
2025-03-14 09:30:49 -0300

    MVC parte final

---
+++ 10_MVC/10_enviando_dados_atualizacao/controllers/TaskController.js
@@ -0,0 +1,64 @@
+const Task = require('../models/Task')
+
+module.exports = class TaskController {
+    static createTask(req, res) {
+        res.render('tasks/create')
+    }
+
+    static async createTaskSave(req, res) {
+        const task = {
+            title: req.body.title,
+            description: req.body.description,
+            done: false //A tarefa já começa como falsa, ou seja, incompleta.
+        }
+
+        //Posso fazer validações
+        //Processar dados. Exemplo: retirar os caracteres especiais.
+
+        //Estou salvando no banco
+        await Task.create(task)
+
+        res.redirect('/tasks')
+    }
+
+    static async removeTask(req, res) {
+
+        const id = req.body.id
+
+        //Método do sequelize que realiza remoção de uma tarefa no banco
+        await Task.destroy({where: {id: id}})
+
+        res.redirect('/tasks')
+
+    }
+
+    static async updateTask(req, res) {
+        const id = req.params.id
+
+        //Método do sequelize que retorna um dado pelo id
+        const task = await Task.findOne({where: {id: id}, raw: true})
+
+        res.render('tasks/edit', { task })
+    }
+
+    static async updateTaskPost(req, res) {
+        const id = req.body.id
+
+        const task = {
+            title: req.body.title,
+            description: req.body.description
+        }
+
+        //Método do sequelize que salva a task editada no banco
+        await Task.update(task, {where: {id}})
+
+        res.redirect('/tasks')
+    }
+
+    static async showTasks(req, res) {
+        //Resgatando todas as tarefas
+        const tasks = await Task.findAll({raw: true})
+
+        res.render('tasks/all', {tasks})
+    }
+}
---
+++ 10_MVC/8_deletando_dados/routes/tasksRoutes.js
@@ -0,0 +1,15 @@
+//Arquivo contempla as rotas das tasks
+
+const express = require('express')
+const router = express.Router()
+
+const TaskController = require('../controllers/TaskController')
+
+router.get('/add', TaskController.createTask)
+router.post('/add', TaskController.createTaskSave)
+router.post('/remove', TaskController.removeTask)
+router.get('/', TaskController.showTasks)
+
+module.exports = router
+
+
