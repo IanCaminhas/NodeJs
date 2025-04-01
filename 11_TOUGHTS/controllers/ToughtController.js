92357f390458df35727b2303630a5b6091491360
Ian Pereira Caminhas <iancaminhas25@hotmail.com>
2025-04-01 13:27:57 -0300

    Tought parte 04

--- 11_TOUGHTS/controllers/ToughtController.js
+++ 11_TOUGHTS/controllers/ToughtController.js
@@ -7,7 +7,37 @@ module.exports = class ToughtsController {
     }
 
     static async dashboard(req, res) {
-        res.render('toughts/dashboard')
+        const userId = req.session.userid
+
+        const user = await User.findOne({
+            where: {
+                id: userId,
+            },
+            include: Tought, //Vai buscar todos os toughts(pensamentos) relacionados ao usuário. É o mesmo que o where do sql
+            plain: true, 
+        })
+
+        /*Vai pegar apenas os dados.
+        Se eu não fizer essa estrutura, outros dados virão juntos. 
+        Em suma, eu estou ignorando os dados que não preciso
+        Basta fazer um console.log() para ver todos os dados.
+        (result) => result.datavalues -> Só vai retornar o que existe em dataValues.
+        Em dataValues estão os dados de pensamento.
+        */
+       const toughts = user.Toughts.map((result) => result.dataValues)
+
+        //Se o usuário não existir, vou redirecioná-lo para a página de login
+        if(!user) {
+            res.redirect('/login')
+        }
+
+        let emptyToughts = false
+
+        if(toughts.length === 0) {
+            emptyToughts = true
+        }
+
+        res.render('toughts/dashboard', {toughts, emptyToughts})
     }
 
     static createTought(req, res) {
@@ -32,6 +62,55 @@ module.exports = class ToughtsController {
        } catch (error) {
             console.log('Aconteceu um erro:'+ error)
        }
-        
     }
+
+    static async removeTought(req, res) {
+        const id = req.body.id
+        const userId = req.session.userid
+
+        try {
+            //UserId: UserId... Significa que o userId precisa ser igual ao UserId da session
+            await Tought.destroy({where: {id: id}, UserId: userId})
+            req.flash('message', 'Pensamento removido com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+        } catch(error) {
+            console.log('Aconteceu um erro: ' + error)
+        }
+       
+    }
+
+    static async updateTought(req, res) {
+        const id = req.params.id
+
+        const tought = await Tought.findOne({where: {id: id}, raw: true})
+
+        res.render('toughts/edit', {tought})
+    }
+
+    static async updateToughtSave(req, res) {
+
+        const id = req.body.id
+
+        const tought = {
+            title: req.body.title
+        }
+
+        try {
+
+            await Tought.update(tought, {where: {id: id}})
+            req.flash('message', 'Pensamento atualizado com sucesso!')
+    
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+
+        } catch(error) {
+            console.log('Aconteceu um erro:' + error)
+        }
+
+    }
+
 }
